02da5ec430da966112b0823e9970b75fedc417e6
Felipe Almeida <felipe.almeida@acqio.com.br>
2023-05-15 11:19:39 -0300

    faet: working external dependency yeey

--- bazelapp/src/main/Main.java
+++ bazelapp/src/main/Main.java
@@ -1,10 +1,14 @@
 import greeting.Greeting;
+import org.apache.commons.lang3.StringUtils;
 
 public class Main {
 
     public static void main(String[] args){
-
         System.out.println("Hello, World!");
         Greeting.greet();
+
+        String myString = "notemptystring";
+        boolean isEmpty = StringUtils.isEmpty(myString);
+        System.out.println(isEmpty);
     }
 }
